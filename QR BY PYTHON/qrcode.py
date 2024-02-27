dc3dc84bd4af1f5a33796e1b9917a42f0500f75a
lv <lvg7338@gmail.com>
2024-02-27 19:20:57 +0530

    updated

--- QR BY PYTHON/qrcode.py
+++ QR BY PYTHON/qrcode.py
@@ -2,7 +2,7 @@ import segno
 
 link = input("Enter the link: ")
 qrc = segno.make_qr(link)
-qrc.save(".qrcode.png",
+qrc.save("qrcode.png",
           scale = 5,
             light = "gainsboro",
             dark = "midnightblue",
